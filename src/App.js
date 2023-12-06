9d80a9bb7213030e8d439d5a97247fae44059c60
Tanvir Ahmed Talukder <rahidt78@gmail.com>
2023-12-06 19:53:51 +0600

    Build basic layout.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,39 @@
-/* 
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
 ];
-*/
+
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />;
+      <Form />;
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 Far Away 👜</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>💼 You've X item on your list and you already packed X (X%)</em>
+    </footer>
+  );
+}
