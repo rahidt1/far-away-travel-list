d59c7919bef900ab0fdec939a4683fb5b4dc1400
Tanvir Ahmed Talukder <rahidt78@gmail.com>
2023-12-06 20:08:16 +0600

    Render static item list.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charge", quantity: 1, packed: true },
 ];
 
 export default function App() {
@@ -27,7 +28,26 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+function Item({ item }) {
+  return (
+    <li>
+      <input type="checkbox" />
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
