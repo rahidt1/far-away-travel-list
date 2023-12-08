b9a8286a03e861246e16c79756b75c160806e632
Tanvir Ahmed Talukder <rahidt78@gmail.com>
2023-12-08 11:48:02 +0600

    Implement sort feature.

--- src/App.js
+++ src/App.js
@@ -89,10 +89,26 @@ Creating N element in an array
 }
 
 function PackingList({ items, onDeleteItem, onToggleItem }) {
+  const [sortBy, setSortBy] = useState("input");
+
+  // Derived state -> calculate from items state
+  let sortedItems;
+  if (sortBy === "input") sortedItems = items;
+
+  if (sortBy === "description")
+    sortedItems = [...items].sort(
+      (a, b) => a.description.localeCompare(b.description) // Sorting strings with non-ASCII characters
+    );
+
+  if (sortBy === "packed")
+    sortedItems = [...items].sort(
+      (a, b) => Number(a.packed) - Number(b.packed) // False value (unpacked) first
+    );
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             key={item.id}
@@ -101,6 +117,14 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
           />
         ))}
       </ul>
+
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
